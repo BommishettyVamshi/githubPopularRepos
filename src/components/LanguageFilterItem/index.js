a47564aae77f44ab9818b63972990b664b7c7602
Bommishetty Vamshi <bvamshivamshi22@gmail.com>
2025-06-30 18:33:36 +0000

    completed the code of  LanguageFilterItem Component and succesfully integrated into the GithubPopularRepos component and verified successfully in the development output

--- src/components/LanguageFilterItem/index.js
+++ src/components/LanguageFilterItem/index.js
@@ -1,1 +1,26 @@
 // Write your code here
+import './index.css'
+
+const LanguageFilterItem = porps => {
+  const {languageFilterDetails, changeLanguage, isActiveLanguage} = porps
+  const {id, language} = languageFilterDetails
+  const activeClassName = isActiveLanguage ? 'activeid' : ''
+
+  const onclickHandler = () => {
+    changeLanguage(id)
+  }
+
+  return (
+    <li className="language-item">
+      <button
+        type="button"
+        className={`language-button ${activeClassName}`}
+        onClick={onclickHandler}
+      >
+        {language}
+      </button>
+    </li>
+  )
+}
+
+export default LanguageFilterItem
