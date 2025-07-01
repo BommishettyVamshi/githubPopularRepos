d6397121d5f249778c515e6fc346f81a58e087a9
Bommishetty Vamshi <bvamshivamshi22@gmail.com>
2025-07-01 09:47:10 +0000

    completed the code of  RepositoryItem Component and succesfully integrated into the GithubPopularRepos component and verified successfully in the development output

--- src/components/RepositoryItem/index.js
+++ src/components/RepositoryItem/index.js
@@ -1,1 +1,41 @@
 // Write your code here
+
+import './index.css'
+
+const RepositoryItem = props => {
+  const {repoDetails} = props
+  const {name, issuesCount, forksCount, starsCount, avatarUrl} = repoDetails
+
+  return (
+    <li className="repo-item">
+      <img src={avatarUrl} className="avatar" alt={name} />
+      <h1 className="name">{name}</h1>
+      <div className="description-card">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/stars-count-img.png"
+          className="icon"
+          alt="stars"
+        />
+        <p className="count">{starsCount} stars</p>
+      </div>
+      <div className="description-card">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/forks-count-img.png"
+          className="icon"
+          alt="forks"
+        />
+        <p className="count">{forksCount} forks</p>
+      </div>
+      <div className="description-card">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/issues-count-img.png"
+          className="icon"
+          alt="open issues"
+        />
+        <p className="count">{issuesCount} open issues</p>
+      </div>
+    </li>
+  )
+}
+
+export default RepositoryItem
