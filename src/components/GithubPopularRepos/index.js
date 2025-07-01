1ed8bb6c351bd64e380589485d42e4ff596de764
Bommishetty Vamshi <bvamshivamshi22@gmail.com>
2025-07-01 09:57:25 +0000

    Successfully Integrated the RepositoryItem component into the GithubPopularRepos component and handled the Loading, Success and Failure states of the API calls in the GithubPopularRepos component and verified in the development output(environment)

--- src/components/GithubPopularRepos/index.js
+++ src/components/GithubPopularRepos/index.js
@@ -1,6 +1,8 @@
 import {Component} from 'react'
 import Loader from 'react-loader-spinner'
 import LanguageFilterItem from '../LanguageFilterItem'
+import RepoistoryItem from '../RepositoryItem'
+
 import './index.css'
 
 const languageFiltersData = [
@@ -11,11 +13,18 @@ const languageFiltersData = [
   {id: 'CSS', language: 'CSS'},
 ]
 
+const apiStatusConstants = {
+  loading: 'Loading',
+  success: 'Success',
+  failure: 'Failure',
+}
+
 // Write your code here
 class GithubPopularRepos extends Component {
   state = {
     activeOptionId: languageFiltersData[0].id,
     githubPopularReposList: [],
+    status: apiStatusConstants.loading,
   }
 
   componentDidMount() {
@@ -25,23 +34,43 @@ class GithubPopularRepos extends Component {
   getPopularRepos = async () => {
     const {activeOptionId} = this.state
 
-    const url = `https://apis.ccbp.in/popular-repos?language=${activeOptionId}`
-
-    const response = await fetch(url)
-    const data = await response.json()
-
-    const formattedData = data.popular_repos.map(each => ({
-      id: each.id,
-      name: each.name,
-      issuesCount: each.issues_count,
-      forksCount: each.forks_count,
-      starsCount: each.stars_count,
-      avatarUrl: each.avatar_url,
-    }))
-
+    // setting the status to loading to appear the loader when filtering the language
     this.setState({
-      githubPopularReposList: formattedData,
+      status: apiStatusConstants.loading,
     })
+
+    try {
+      const url = `https://apis.ccbp.in/popular-repos?language=${activeOptionId}`
+
+      // obtaing the response from server to the url (get method)
+      const response = await fetch(url)
+
+      // checking and handling response (for the erros like : bad url, server side errors etc)
+      if (!response.ok) {
+        this.setState({
+          status: apiStatusConstants.failure,
+        })
+        return
+      }
+      const data = await response.json()
+      const formattedData = data.popular_repos.map(each => ({
+        id: each.id,
+        name: each.name,
+        issuesCount: each.issues_count,
+        forksCount: each.forks_count,
+        starsCount: each.stars_count,
+        avatarUrl: each.avatar_url,
+      }))
+      this.setState({
+        githubPopularReposList: formattedData,
+        status: apiStatusConstants.success,
+      })
+    } catch (error) {
+      // checking and handling the errors like(internet error, DNS error, CORS errors etc.)
+      this.setState({
+        status: apiStatusConstants.failure,
+      })
+    }
   }
 
   changeLanguage = languageId => {
@@ -53,9 +82,36 @@ class GithubPopularRepos extends Component {
     )
   }
 
-  render() {
-    const {activeOptionId, githubPopularReposList} = this.state
-    console.log(activeOptionId, githubPopularReposList)
+  renderLoader = () => (
+    <div data-testid="loader" className="loader-card">
+      <Loader type="ThreeDots" color="#0284c7" height={80} width={80} />
+    </div>
+  )
+
+  renderSuccess = () => {
+    const {githubPopularReposList} = this.state
+    return (
+      <ul className="repos-list-container">
+        {githubPopularReposList.map(eachRepo => (
+          <RepoistoryItem repoDetails={eachRepo} key={eachRepo.id} />
+        ))}
+      </ul>
+    )
+  }
+
+  renderFailure = () => (
+    <>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/api-failure-view.png"
+        className="failure-view"
+        alt="failure view"
+      />
+      <h1>Something Went Wrong</h1>
+    </>
+  )
+
+  renderPopularGitRepos = () => {
+    const {activeOptionId, status} = this.state
     return (
       <div className="app-container">
         <h1 className="heading">Popular</h1>
@@ -69,8 +125,15 @@ class GithubPopularRepos extends Component {
             />
           ))}
         </ul>
+        {status === apiStatusConstants.loading && this.renderLoader()}
+        {status === apiStatusConstants.success && this.renderSuccess()}
+        {status === apiStatusConstants.failure && this.renderFailure()}
       </div>
     )
+  }
+
+  render() {
+    return this.renderPopularGitRepos()
   }
 }
 
